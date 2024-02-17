0203e6c8573746b05e612234c8e9d2f8a5a5c9c5
Evan Fadhilla <evfadhw@gmail.com>
2024-02-17 09:44:19 +0700

    add get method

--- main.go
+++ main.go
@@ -13,6 +13,13 @@ type CreateRequest struct {
 	Description string `json:"description"`
 }
 
+type TodoResponse struct {
+	Id int `json:"id"`
+	Title string `json:"title"`
+	Description string `json:"description"`
+	Done bool `json:"done"`
+}
+
 func main() {
 	db := database.InitDB()
 	defer db.Close()
@@ -23,6 +30,45 @@ func main() {
 	 }
 	e := echo.New()
 
+	e.GET("/todos", func(ctx echo.Context) error {
+
+		rows , err := db.Query("SELECT * FROM todos")
+		if err != nil {
+			panic(err)
+		}
+		var res []TodoResponse
+
+		for rows.Next() {
+			var id int
+			var title string
+			var description string
+			var done int
+			err = rows.Scan(&id, &title, &description, &done)
+
+			if err != nil {
+				return ctx.String(http.StatusInternalServerError, err.Error())
+			}
+
+			var todo TodoResponse
+			todo.Id = id
+			todo.Title = title
+			todo.Description = description
+			if done == 1 {
+				todo.Done = true
+			}
+
+			res = append(res, todo)
+
+			
+			
+			
+		}
+		return ctx.JSON(http.StatusOK, res)
+	})
+
+
+
+
 	e.POST("/todos", func(ctx echo.Context) error {
 
 		var request CreateRequest
@@ -32,13 +78,15 @@ func main() {
 			"INSERT INTO todos (title, description, done) VALUES (?,?, 0)",
 			request.Title, request.Description,
 		)
-
+s
 		if err != nil {
 			return ctx.String(http.StatusInternalServerError, err.Error())
 		}
 
 		return ctx.String(http.StatusOK, "OK")
 	})
+
+
 
 	e.Start(":8080")
 
